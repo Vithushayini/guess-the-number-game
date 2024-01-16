bb0707549ef337e976c9fb0d9dc6632108980817
Vithushayini <139032072+Vithushayini@users.noreply.github.com>
2024-01-16 07:48:31 +0530

    head div added to App.js and add style to it

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      
+      <div className='head'>
+          <label htmlFor='term'>Guess the number between 1 to 10</label>
+      </div>
+      <input
+        id="term"
+        type='text'
+        name='term'
+      />
     </div>
   );
 }
