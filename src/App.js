778ee6961a94c5d0b6d785940dcc6bfb3a77322d
Vithushayini <139032072+Vithushayini@users.noreply.github.com>
2024-01-16 07:55:52 +0530

    added onchange function using state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[term,setTerm]=useState("");
+  
   return (
     <div className="container">
       <div className='head'>
           <label htmlFor='term'>Guess the number between 1 to 10</label>
       </div>
-      <input
+      <input onChange={(event)=>{
+        setTerm(event.target.value)
+      }}
         id="term"
         type='text'
         name='term'
