bbea30ba56fcc91bb61a7769e4e1d19305d16fa4
Vithushayini <139032072+Vithushayini@users.noreply.github.com>
2024-01-16 08:19:05 +0530

    game's logic added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import Result from './Result';
+const secretNum=Math.floor(Math.random()*10)+1;
 
 function App() {
   const[term,setTerm]=useState("");
-  
+
   return (
     <div className="container">
       <div className='head'>
-          <label htmlFor='term'>Guess the number between 1 to 10</label>
+          <label htmlFor='term'>Guess the number between 1 to 10 </label>
       </div>
       <input onChange={(event)=>{
         setTerm(event.target.value)
@@ -17,6 +19,8 @@ function App() {
         type='text'
         name='term'
       />
+      <Result secretNum={secretNum} 
+      term={term}/>
     </div>
   );
 }
